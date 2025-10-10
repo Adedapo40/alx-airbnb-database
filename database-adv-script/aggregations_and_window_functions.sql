f1cac9cd46a5a551a0127c8dfd6c503bd36c24e5
Adedapo Ayanyemi <123646741+Adedapo40@users.noreply.github.com>
2025-10-10 12:12:18 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,10 +9,11 @@ GROUP BY     Users.user_id, Users.first_name
 ORDER BY total_bookings DESC;
 
 
+
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number
 FROM Booking
 GROUP BY property_id
-ORDER BY booking_rank;
+ORDER BY row_number;
