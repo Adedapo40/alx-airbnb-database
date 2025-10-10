c913bfb30e1ec93d3ef62de3e2a2025b9716dce0
Adedapo Ayanyemi <123646741+Adedapo40@users.noreply.github.com>
2025-10-10 11:13:44 +0100

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,14 @@
+SELECT * FROM Property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
 
+SELECT * FROM Users
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = Users.user_id
+) > 3;
